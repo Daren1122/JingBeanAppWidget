4aa4962f78a086a91d7e7b063f552940a420623d
wangjing <1029@qq.com>
2021-10-25 11:57:47 +0800

    move

--- app/src/main/java/com/wj/jd/util/Main.kt
+++ app/src/main/java/com/wj/jd/util/Main.kt
@@ -1,5 +1,6 @@
 package com.wj.jd.util
 
+import android.graphics.Color
 import android.util.Log
 
 /**
@@ -8,11 +9,23 @@ import android.util.Log
  * Description
  */
 fun main() {
-    colorToHexValue()
+    transColor("#ffffc000")
 }
 
 fun colorToHexValue() {
     val d = Integer.valueOf("ff", 16)
     println(d.toString())
+}
+
+fun transColor(color: String): Int {
+    val c1 = color.subSequence(color.length - 6, color.length - 4)
+    val c2 = color.subSequence(color.length - 4, color.length - 2)
+    val c3 = color.subSequence(color.length - 2, color.length)
+
+    val d1 = Integer.valueOf(c1.toString(), 16)
+    val d2 = Integer.valueOf(c2.toString(), 16)
+    val d3 = Integer.valueOf(c3.toString(), 16)
+
+    return Color.rgb(255 - d1, 255 - d2, 255 - d3)
 }
 
