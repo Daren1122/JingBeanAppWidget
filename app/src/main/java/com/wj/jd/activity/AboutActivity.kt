658d4a5a9a9be48b1d37cce36ccafcaf2b8b5e29
wangjing <1029@qq.com>
2021-10-26 10:58:43 +0800

    move

--- app/src/main/java/com/wj/jd/activity/AboutActivity.kt
+++ app/src/main/java/com/wj/jd/activity/AboutActivity.kt
@@ -27,9 +27,5 @@ class AboutActivity : BaseActivity() {
             val intent = Intent.parseUri("intent://platformapi/startapp?saId=10000007&clientVersion=3.7.0.0718&qrcode=https%3A%2F%2Fqr.alipay.com%2F{urlCode}%3F_s%3Dweb-other&_t=1472443966571#Intent;scheme=alipayqr;package=com.eg.android.AlipayGphone;end".replace("{urlCode}", urlCode), 1)
             startActivity(intent);
         }
-
-        weChatPay.setOnClickListener {
-
-        }
     }
 }
